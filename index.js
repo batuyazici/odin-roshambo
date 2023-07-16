2375c42c5c8e14f24c3e533b3844c234ca98c2cf
Batuhan Yazici <92368839+batuyazici@users.noreply.github.com>
2023-07-16 13:41:47 +0300

    Add functions

---
+++ index.js
@@ -0,0 +1,26 @@
+function getComputerChoice() {
+    let choice =  Math.floor(Math.random() * 3) + 1;
+    switch (choice) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+        default:
+            return null;
+    }    
+}
+let numRound = 5;
+let playerSelection, computerSelection;
+
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+function game() {
+for (let i = 0; i < numRound; i++) {
+     
+}
+}
